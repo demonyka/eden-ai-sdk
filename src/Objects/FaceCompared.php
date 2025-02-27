5f7bf8adc1b72207299fb7d4d7afa85b9c86e8a2
demonyga <cweranit@gmail.com>
2025-02-27 08:48:31 +0400

    Face Compare fix

--- src/Objects/FaceCompared.php
+++ src/Objects/FaceCompared.php
@@ -10,4 +10,24 @@ class FaceCompared extends BaseObject
             'providers' => $this->providers,
         ];
     }
+
+    public function getAverageConfidence(): float|int
+    {
+        $totalConfidence = 0;
+        $count = 0;
+
+        foreach ($this->providers as $provider) {
+            if ($provider['status'] !== 'success') continue;
+            foreach ($provider['items'] as $item) {
+                $totalConfidence += $item['confidence'];
+                $count++;
+            }
+        }
+
+        if ($count === 0) {
+            return 0;
+        }
+
+        return $totalConfidence / $count;
+    }
 }
